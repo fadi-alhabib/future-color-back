53e38b0753c29cef55895cea32559ddf0323436c
jesus is king <fadi.b.alhabib@gmail.com>
2025-05-15 21:12:42 +0300

    final

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/storage/{folder}/{filename}', function ($folder, $filename) {
+    $path = storage_path('app/public/' . $folder . '/' . $filename);
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    return response()->file($path);
 });
+
+
+Route::get('{any}', function () {
+    return redirect("https://futureclr.com", secure: true);
+})->where('any', '.*');
